065dc5c497dc18a3c47d8e44838761a5f35098f4
Amit Kadam <amitkadam2952@gmail.com>
2025-09-27 18:12:27 +0530

    Initial commit

--- Day6_Apicall.js
+++ Day6_Apicall.js
@@ -1,16 +1,43 @@
-//async : inside getData await is used
+// //async : inside getData await is used
 
-async function getData()  {
-// call api for data  https://dummyjson.com/todos/1 
+// async function getData()  {
+// // call api for data  https://dummyjson.com/todos/1 
 
-// await : it will take some time to fetch api response
+// // await : it will take some time to fetch api response
 
-let rawData =  await fetch ('https://dummyjson.com/todos/1');
+// let rawData =  await fetch ('https://dummyjson.com/todos/1');
 
-//await : it will take some time to phrase rawData ti json
+// //await : it will take some time to phrase rawData t0 json
 
-let res = await rawData.json();
-console.log(res);
+// let res = await rawData.json();
+// console.log(res);
 
+// }
+// getData();
+
+console.log("============================= TRY & CATCH =========================================");
+
+
+async function getData2() {
+    
 }
-getData();
+try {
+    let rawData = await fetch("https://dummyjson.com/tod");
+    let res = await rawData.json();
+    console.log(res);
+} catch (error) {
+    console.log("somethong went wrong");
+    
+}
+getData2();
+
+// console.log("============================= THEN & CATCH ============================================");
+
+// function getData2()
+// {
+//     fetch("https://dummyjson.com/todo")
+//     .then((rawData)=>rawData.json())
+//     .then((data)=>console.log(data))
+//     .catch((e)=>console.log("error"));
+// }
+// getData2();
